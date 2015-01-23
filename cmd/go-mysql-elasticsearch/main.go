ee33364f6c6473a59ed5744e9e88f0b3a6ff51c7
siddontang <siddontang@gmail.com>
2015-01-23 09:38:21 +0800

    add flag for convenience use

--- cmd/go-mysql-elasticsearch/main.go
+++ cmd/go-mysql-elasticsearch/main.go
@@ -11,8 +11,13 @@ import (
 
 var configFile = flag.String("config", "./etc/river.toml", "go-mysql-elasticsearch config file")
 var my_addr = flag.String("my_addr", "", "MySQL addr")
+var my_user = flag.String("my_user", "", "MySQL user")
+var my_pass = flag.String("my_pass", "", "MySQL password")
 var es_addr = flag.String("es_addr", "", "Elasticsearch addr")
 var data_dir = flag.String("data_dir", "", "path for go-mysql-elasticsearch to save data")
+var server_id = flag.Int("server_id", 0, "MySQL server id, as a pseudo slave")
+var flavor = flag.String("flavor", "", "flavor: mysql or mariadb")
+var execution = flag.String("exec", "", "mysqldump execution path")
 
 func main() {
 	runtime.GOMAXPROCS(runtime.NumCPU())
@@ -37,12 +42,32 @@ func main() {
 		cfg.MyAddr = *my_addr
 	}
 
+	if len(*my_user) > 0 {
+		cfg.MyUser = *my_user
+	}
+
+	if len(*my_pass) > 0 {
+		cfg.MyPassword = *my_pass
+	}
+
+	if *server_id > 0 {
+		cfg.ServerID = uint32(*server_id)
+	}
+
 	if len(*es_addr) > 0 {
 		cfg.ESAddr = *es_addr
 	}
 
 	if len(*data_dir) > 0 {
 		cfg.DataDir = *data_dir
+	}
+
+	if len(*flavor) > 0 {
+		cfg.Flavor = *flavor
+	}
+
+	if len(*execution) > 0 {
+		cfg.DumpExec = *execution
 	}
 
 	r, err := river.NewRiver(cfg)
