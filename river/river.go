a76d30fb43c21f7d8545690078da69afe3d5382f
WangXiangUSTC <xiang13225080@163.com>
2017-04-12 20:20:07 +0800

    change Id to ID

--- river/river.go
+++ river/river.go
@@ -212,7 +212,7 @@ func (r *River) prepareRule() error {
 					rr.Index = rule.Index
 					rr.Type = rule.Type
 					rr.Parent = rule.Parent
-					rr.Id = rule.Id
+					rr.ID = rule.ID
 					rr.FieldMapping = rule.FieldMapping
 				}
 			} else {
