82de3f84309f00cb835b60a20bbfaac6a39e199d
WangXiangUSTC <347249478@qq.com>
2017-06-13 23:24:48 +0800

    add assert of response code, and fix the bug of parent test

--- elastic/client_test.go
+++ elastic/client_test.go
@@ -53,6 +53,7 @@ func (s *elasticTestSuite) TestSimple(c *C) {
 
 	r, err := s.c.Get(index, docType, "1")
 	c.Assert(err, IsNil)
+	c.Assert(r.Code, Equals, 200)
 	c.Assert(r.ID, Equals, "1")
 
 	err = s.c.Delete(index, docType, "1")
@@ -78,6 +79,7 @@ func (s *elasticTestSuite) TestSimple(c *C) {
 
 	resp, err := s.c.IndexTypeBulk(index, docType, items)
 	c.Assert(err, IsNil)
+	c.Assert(resp.Code, Equals, 200)
 	c.Assert(resp.Errors, Equals, false)
 
 	for i := 0; i < 10; i++ {
@@ -90,6 +92,7 @@ func (s *elasticTestSuite) TestSimple(c *C) {
 
 	resp, err = s.c.IndexTypeBulk(index, docType, items)
 	c.Assert(err, IsNil)
+	c.Assert(resp.Code, Equals, 200)
 	c.Assert(resp.Errors, Equals, false)
 }
 
@@ -112,16 +115,20 @@ func (s *elasticTestSuite) TestParent(c *C) {
 
 	resp, err := s.c.IndexTypeBulk(index, docType, items)
 	c.Assert(err, IsNil)
+	c.Assert(resp.Code, Equals, 200)
 	c.Assert(resp.Errors, Equals, false)
-
 	for i := 0; i < 10; i++ {
 		id := fmt.Sprintf("%d", i)
 		req := new(BulkRequest)
+		req.Index = index
+		req.Type = docType
 		req.Action = ActionDelete
 		req.ID = id
+		req.Parent = "1"
 		items[i] = req
 	}
-	resp, err = s.c.IndexTypeBulk(index, docType, items)
+	resp, err = s.c.Bulk(items)
 	c.Assert(err, IsNil)
+	c.Assert(resp.Code, Equals, 200)
 	c.Assert(resp.Errors, Equals, false)
 }
