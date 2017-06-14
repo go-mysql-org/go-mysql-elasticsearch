98c14502a793f5632949693eb8d7cdc269afb65f
WangXiangUSTC <xiang13225080@163.com>
2017-06-14 09:51:41 +0800

    add assert of response code, and fix the bug of parent test (#110)

--- elastic/client_test.go
+++ elastic/client_test.go
@@ -53,6 +53,7 @@ func (s *elasticTestSuite) TestSimple(c *C) {
 
 	r, err := s.c.Get(index, docType, "1")
 	c.Assert(err, IsNil)
+	c.Assert(r.Code, Equals, 200)
 	c.Assert(r.ID, Equals, "1")
 
 	err = s.c.Delete(index, docType, "1")
@@ -78,6 +79,7 @@ func (s *elasticTestSuite) TestSimple(c *C) {
 
 	resp, err := s.c.IndexTypeBulk(index, docType, items)
 	c.Assert(err, IsNil)
+	c.Assert(resp.Code, Equals, 200)
 	c.Assert(resp.Errors, Equals, false)
 
 	for i := 0; i < 10; i++ {
@@ -90,6 +92,7 @@ func (s *elasticTestSuite) TestSimple(c *C) {
 
 	resp, err = s.c.IndexTypeBulk(index, docType, items)
 	c.Assert(err, IsNil)
+	c.Assert(resp.Code, Equals, 200)
 	c.Assert(resp.Errors, Equals, false)
 }
 
@@ -97,9 +100,18 @@ func (s *elasticTestSuite) TestSimple(c *C) {
 func (s *elasticTestSuite) TestParent(c *C) {
 	index := "dummy"
 	docType := "comment"
+	ParentType := "parent"
 
+	mapping := map[string]interface{}{
+			docType: map[string]interface{}{
+				"_parent": map[string]string{"type": ParentType},
+		},
+	}
+	err := s.c.CreateMapping(index, docType,  mapping)
+	c.Assert(err, IsNil)
+	
 	items := make([]*BulkRequest, 10)
-
+	
 	for i := 0; i < 10; i++ {
 		id := fmt.Sprintf("%d", i)
 		req := new(BulkRequest)
@@ -112,16 +124,20 @@ func (s *elasticTestSuite) TestParent(c *C) {
 
 	resp, err := s.c.IndexTypeBulk(index, docType, items)
 	c.Assert(err, IsNil)
+	c.Assert(resp.Code, Equals, 200)
 	c.Assert(resp.Errors, Equals, false)
-
 	for i := 0; i < 10; i++ {
 		id := fmt.Sprintf("%d", i)
 		req := new(BulkRequest)
+		req.Index = index
+		req.Type = docType
 		req.Action = ActionDelete
 		req.ID = id
+		req.Parent = "1"
 		items[i] = req
 	}
-	resp, err = s.c.IndexTypeBulk(index, docType, items)
+	resp, err = s.c.Bulk(items)
 	c.Assert(err, IsNil)
+	c.Assert(resp.Code, Equals, 200)
 	c.Assert(resp.Errors, Equals, false)
 }
