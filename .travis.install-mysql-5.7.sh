c82a1d6af9e41a5b0a40d4b588c8d7e84d623941
WangXiangUSTC <347249478@qq.com>
2017-06-25 20:18:44 +0800

    try to support mysql 5.7 in travis ci

--- .travis.install-mysql-5.7.sh
+++ .travis.install-mysql-5.7.sh
@@ -1,5 +1,5 @@
 mysql --version
-echo mysql-apt-config mysql-apt-config/select-server select mysql-5.7 | sudo debconf-set-selections
+echo mysql-apt-config mysql-apt-config/select-server select mysql-5.6 | sudo debconf-set-selections
 wget http://dev.mysql.com/get/mysql-apt-config_0.7.3-1_all.deb
 sudo dpkg --install mysql-apt-config_0.7.3-1_all.deb
 sudo apt-get update -q
