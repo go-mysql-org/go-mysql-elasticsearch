a9977d49624ac79792599bf03535eb7a65ffbbfa
Nicholas Terrell <nicholas.terrell@mnfgropup.limited>
2018-09-06 16:22:38 +1000

    Added a zero date check for datetime and timezone. Added parser for date fields with a zero date check

--- river/sync.go
+++ river/sync.go
@@ -333,8 +333,21 @@ func (r *River) makeReqColumnData(col *schema.TableColumn, value interface{}) in
 	case schema.TYPE_DATETIME, schema.TYPE_TIMESTAMP:
 		switch v := value.(type) {
 		case string:
-			vt, _ := time.ParseInLocation(mysql.TimeFormat, string(v), time.Local)
+			vt, err := time.ParseInLocation(mysql.TimeFormat, string(v), time.Local)
+			if err != nil || vt.IsZero() { // failed to parse date or zero date
+				return nil
+			}
 			return vt.Format(time.RFC3339)
+		}
+	case schema.TYPE_DATE:
+		switch v := value.(type) {
+		case string:
+			dateFormat := "2006-01-02"
+			vt, err := time.Parse(dateFormat, string(v))
+			if err != nil || vt.IsZero() { // failed to parse date or zero date
+				return nil
+			}
+			return vt.Format(dateFormat)
 		}
 	}
 
