99e1dd0930bab454d4f4c9b6e72456725bfadaa1
王社英 <tiankonghewo@gmail.com>
2018-11-21 15:58:58 +0800

    Update sync.go

--- river/sync.go
+++ river/sync.go
@@ -519,7 +519,10 @@ func (r *River) getFieldValue(col *schema.TableColumn, fieldType string, value i
 		        col.Type = schema.TYPE_DATETIME
 		        v := r.makeReqColumnData(col, value)
                         str, _ := v.(string)
-		        stamp, _ := time.ParseInLocation("2006-01-02 03:04:05", str, time.Local)  
+			  
+		        //stamp, _ := time.ParseInLocation("2006-01-02 03:04:05", str, time.Local) 
+			// time.RFC3339 解析的时间格式是 2018-10-30T01:45:00Z
+			stamp, _ := time.ParseInLocation(time.RFC3339, str, time.Local)
 		        t := int64(stamp.Unix())
 			fieldValue = r.makeReqColumnData(col, time.Unix(t, 0).Format(mysql.TimeFormat))
 		    }
